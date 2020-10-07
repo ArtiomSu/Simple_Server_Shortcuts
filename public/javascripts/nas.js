7d3e1c3e1d12d4db86121ad20e72635b9622bde5
ArtiomSu <artiom.sumigora@mycit.ie>
2020-10-07 01:38:00 +0100

    added ability to loop device info

--- public/javascripts/nas.js
+++ public/javascripts/nas.js
@@ -3,7 +3,22 @@ window.onload=function (){
     var update_logs = (text) =>{
         logs_div.innerHTML = "<code>"+text+"</code>";
         logs_div.scrollTop = logs_div.scrollHeight;
-    }
+    };
+
+    var get_info = (type) =>{
+        if(type){
+            last_gotten = type;
+        }
+        if(last_gotten === null){
+            last_gotten = "t";
+        }
+        httpGetAsync("/info/"+last_gotten,(text)=>{
+            update_logs(JSON.parse(text).result);
+            if(loop_duration !== 0){
+                setTimeout(get_info,loop_duration*1000);
+            }
+        });
+    };
 
     var get_top = document.getElementById("get_top");
     var get_disk = document.getElementById("get_disk");
@@ -13,30 +28,53 @@ window.onload=function (){
     var logs_div = document.getElementById("results");
     var header = document.getElementById("header_name");
 
+    var loop_one = document.getElementById("loop_one");
+    var loop_five = document.getElementById("loop_five");
+
+    var loop_duration = 0;
+    var last_gotten = null;
+
+
 
     get_top.onclick = function (){
-        httpGetAsync("/info/t",(text)=>{
-            update_logs(JSON.parse(text).result);
-        });
+        get_info("t");
     }
     get_disk.onclick = function (){
-        httpGetAsync("/info/d",(text)=>{
-            update_logs(JSON.parse(text).result);
-        });
+        get_info("d");
     }
     get_temp.onclick = function (){
-        httpGetAsync("/info/c",(text)=>{
-            update_logs(JSON.parse(text).result);
-        });
+        get_info("c");
     }
     get_all.onclick = function (){
-        httpGetAsync("/info/a",(text)=>{
-            update_logs(JSON.parse(text).result);
-        });
+        get_info("a");
     }
 
     header.onclick = function (){
         window.location.href='/';
+    }
+
+    loop_one.onclick = function (){
+        if(loop_duration === 1){
+            loop_duration = 0;
+            loop_one.style.backgroundColor = "darkred";
+        }else{
+            loop_duration = 1;
+            loop_one.style.backgroundColor = "lawngreen";
+            loop_five.style.backgroundColor = "darkred";
+        }
+        get_info();
+    }
+
+    loop_five.onclick = function (){
+        if(loop_duration === 5){
+            loop_duration = 0;
+            loop_five.style.backgroundColor = "darkred";
+        }else{
+            loop_duration = 5;
+            loop_one.style.backgroundColor = "darkred";
+            loop_five.style.backgroundColor = "lawngreen";
+        }
+        get_info()
     }
 
 };
