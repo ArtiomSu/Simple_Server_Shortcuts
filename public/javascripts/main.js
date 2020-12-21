3b212299f30d91b0ff99d310ddc675d852b3e634
ArtiomSu <artiom.sumigora@mycit.ie>
2020-12-21 19:16:31 +0000

    added windows shutdown options

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,12 +8,30 @@ window.onload=function (){
     var telegram_bot_restart_buttom = document.getElementById("refresh_telegram_bot");
     var logs_div = document.getElementById("log_results");
     var test_logs_button = document.getElementById("test_logs_button");
+    var windowsmain_button = document.getElementById("shutdown_windows_laptop");
+    var windows_amd_button = document.getElementById("shutdown_windows_bro");
 
     telegram_bot_restart_buttom.onclick = function (){
         httpGetAsync("http://10.0.0.69:19876/shutdown",(text)=>{
             update_logs("telegram bot restart: "+text);
         });
     }
+
+    windowsmain_button.onclick = function (){
+        httpGetAsync("http://10.0.0.2/shutdown",(text)=>{
+            update_logs("Windows Main Turned off:"+text);
+        });
+        update_logs("Windows Main Turned off");
+    }
+
+    windows_amd_button.onclick = function (){
+        httpGetAsync("http://10.0.0.11/shutdown",(text)=>{
+            update_logs("Windows AMD Turned off:"+text);
+        });
+        update_logs("Windows AMD Turned off");
+    }
+
+
 
     test_logs_button.onclick = function (){
         for(var i = 0; i< 10;i++){
